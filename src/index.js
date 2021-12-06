dcaf0cbf33acde46e8db42277ded7b0b8f789d40
Caleb Rodriguez <sirdigo6@gmail.com>
2021-12-06 13:31:22 -0500

    commit tree 3e2ceb329d3f4e2730d1d4379bd5b1857c990b5b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('%c HI', 'color: firebrick')
+const imageUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Fetching Images
+    fetching(imageUrl)
+    //Fetching Breeds
+    fetching(breedUrl)
+})
+function fetching(link) {
+    let funct;
+    link === imageUrl ? funct = appendImg : funct = breedAdd;
+
+    fetch(link)
+    .then(resp => resp.json())
+    .then(data => funct(data))
+}
+function appendImg(data) {
+    data.message.forEach(img => {
+        const image = document.createElement("img")
+        image.src = img;
+        document.querySelector("#dog-image-container").appendChild(image)
+    });
+}    
+
+
+function breedAdd(data){
+    const list = document.querySelector("#dog-breeds")
+    for (const breed in data.message){
+        const li = document.createElement("li");
+        li.textContent = breed;
+        list.appendChild(li);
+    }
+    list.addEventListener("click", (e) => {
+        e.target.style.color = "#162abc"
+        })
+}
